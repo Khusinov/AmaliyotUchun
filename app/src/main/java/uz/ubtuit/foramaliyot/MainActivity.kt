8d3c2fed7fde33590f55b96ead640d38863130f0
Khusinov <shaxriyorxusinov2020@gmail.com>
2022-08-12 06:50:30 +0500

    First commit

---
+++ app/src/main/java/uz/ubtuit/foramaliyot/MainActivity.kt
@@ -0,0 +1,11 @@
+package uz.ubtuit.foramaliyot
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
